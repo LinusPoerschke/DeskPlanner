998d9fcc5ab419116e8fc10f27f560034f188b8d
LinusPoerschke <linus.poerschke@hotmail.com>
2025-01-21 18:12:47 +0100

    Ordner gelöscht

--- webserver/sensors/raspberry-remote/webinterface/shutdown.php
+++
@@ -1,9 +0,0 @@
-<?php
-        function runShutdown(){
-                exec("/sbin/shutdown -h now");
-        }
-
-        if (isset($_GET['shutdown'])) {
-                runShutdown();
-        }
-?>
